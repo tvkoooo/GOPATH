00ece9dde1209c4138ad828c6b21894aaccd17ee
tvkoooo <huolong-2010@163.com>
2020-03-29 13:19:46 +0800

    bat

--- bin/sqltest.bat
+++ bin/sqltest.bat
@@ -1,5 +1,5 @@
 @echo off
-set c1="tvkoooo:dk2012@tcp(192.168.1.106:3306)/db_mm_cherry?charset=utf8"
+set c1="tvkoooo:dk2012@tcp(127.0.0.1:3306)/db_mm_cherry?charset=utf8"
 set c2="select `id`,`name` from t_user_basic;"
 set c3="select * from t_user_basic;"
 set c4="select * from t_not;"
